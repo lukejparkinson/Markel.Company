58bb7d672909e81196ce84db79fec228cf60ffdb
Luke Parkinson <parkinson.lukej@gmail.com>
2024-01-14 18:48:51 +0000

    required attributes

--- Markel.Company.Domain/ViewModels/ClaimBase.cs
+++ Markel.Company.Domain/ViewModels/ClaimBase.cs
@@ -1,15 +1,23 @@
 ﻿using Markel.Company.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Markel.Company.Domain.ViewModels;
 
 public class ClaimBase
 {
     public Guid Ucr { get; set; }
+    [Required]
     public int CompanyId { get; set; }
+    [Required]
     public DateTime ClaimDate { get; set; }
+    [Required]
     public DateTime LossDate { get; set; }
+    [Required]
     public string? AssuredName { get; set; }
+    [Required]
     public decimal IncurredLoss { get; set; }
+    [Required]
     public bool Closed { get; set; }
+    [Required]
     public ClaimType ClaimType { get; set; }
 }
